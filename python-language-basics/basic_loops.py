686ff312a91c40b40eb0bddcc0056a3374615375
Aarya Dhaliwal <jasdhali@gmail.com>
2026-05-14 00:50:01 -0500

    string-examples/StringEx1.py

---
+++ python-language-basics/basic_loops.py
@@ -0,0 +1,13 @@
+#iterate over a list
+fruits = ["orange","banana","apple"]
+for fruit in fruits:
+    print(fruit)
+    
+
+#Prints numbers 0 to 4
+for i in range(5):
+    print(i)
+
+#Looping through a string    
+for char in "Python":
+    print(char)
---
+++ string-examples/StringEx1.py
@@ -0,0 +1,7 @@
+text = "Pythong ptogramming"
+
+#access single char
+print( text[0])
+print( text[-1])
+
+
