416a8e6e61f71250c6d00c5715106c4a80bfe556
Aarya Dhaliwal <jasdhali@gmail.com>
2026-05-14 00:49:31 -0500

    python-language-basics/basic_loops.py

---
+++ python-language-basics/simple_nums.py
@@ -0,0 +1,8 @@
+num1 = 18
+num2 = 7
+
+#Addition
+sum_val = num1 + num2
+
+#Use f-strings for easy 
+print(f"Sum of {num1} and {num2} is {sum_val}")
---
+++ python-language-basics/user_input.py
@@ -0,0 +1,3 @@
+name= input("Enter your name:")
+birth_year = int(input("Enter your birth year: "))
+print(f"Birth year is -> {birth_year}") 
