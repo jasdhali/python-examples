7b90e22d99975cf251944553e944d99b4da49f67
Aarya Dhaliwal <jasdhali@gmail.com>
2026-06-01 18:40:08 -0500

    various exercises

---
+++ language-practice/convert_boolean.py
@@ -0,0 +1,5 @@
+bool_true = True
+bool_false = False
+
+print(f"True as an integer: {int(bool_true)} ")
+print(f"False as an integer: {int(bool_false)} ")
---
+++ language-practice/list-dictionary-example.py
@@ -0,0 +1,10 @@
+word_list = ["python", "is", "amazing"]
+joined_word_list = ", ".join(word_list)
+print(joined_word_list)
+
+academy_info = {
+    "name": "Some Academy", "age": 6, "language":"python"}
+print(academy_info)
+print(academy_info.values() )
+print(academy_info.keys() )
+print(academy_info["age"] )
